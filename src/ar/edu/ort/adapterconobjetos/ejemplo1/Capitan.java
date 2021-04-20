f2b2835ae3252daf544e44efee827cb8d796ef9d
Marco Cupo <cuttlas88@yahoo.com.ar>
2021-04-19 23:46:09 -0300

    ajustes

--- src/ar/edu/ort/adapterconobjetos/ejemplo1/Capitan.java
+++ src/ar/edu/ort/adapterconobjetos/ejemplo1/Capitan.java
@@ -5,7 +5,14 @@ public class Capitan {
 
 	private BarcoAVela barcoAVela;
 	
+	public Capitan() {
+	}
+	
 	public Capitan(BarcoAVela barcoAVela) {
+		this.barcoAVela = barcoAVela;
+	}
+	
+	public void setbarcoAVela(BarcoAVela barcoAVela) {
 		this.barcoAVela = barcoAVela;
 	}
 
